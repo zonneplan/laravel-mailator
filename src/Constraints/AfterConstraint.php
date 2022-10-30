2d25948ec792cb5eb0ce9394e6f75477285829e9
Dennis Stolmeijer <dstollie@gmail.com>
2022-10-30 17:03:30 +0100

    Fixed functionality of AfterConstraint #2564

--- src/Constraints/AfterConstraint.php
+++ src/Constraints/AfterConstraint.php
@@ -22,9 +22,7 @@ class AfterConstraint implements SendScheduleConstraint
                 return false;
             }
 
-            return $schedule->isOnce()
-                ? $schedule->timestamp_target->diffInDays(now()->floorSeconds()) === $schedule->toDays()
-                : $schedule->timestamp_target->diffInDays(now()->floorSeconds()) > $schedule->toDays();
+            return $schedule->timestamp_target->diffInDays(now()->floorSeconds()) >= $schedule->toDays();
         }
 
         if ($schedule->toHours() > 0) {
@@ -32,19 +30,13 @@ class AfterConstraint implements SendScheduleConstraint
                 return false;
             }
 
-            //till ends we should have at least toDays days
-            return $schedule->isOnce()
-                ? $schedule->timestamp_target->diffInHours(now()->floorSeconds()) === $schedule->toHours()
-                : $schedule->timestamp_target->diffInHours(now()->floorSeconds()) > $schedule->toHours();
+            return $schedule->timestamp_target->diffInHours(now()->floorSeconds()) >= $schedule->toHours();
         }
 
-        if (now()->floorSeconds()->lte($schedule->timestampTarget()->addMinutes($schedule->delay_minutes))) {
+        if (now()->floorSeconds()->lt($schedule->timestampTarget()->addMinutes($schedule->delay_minutes))) {
             return false;
         }
 
-        //till ends we should have at least toDays days
-        return $schedule->isOnce()
-            ? $schedule->timestamp_target->diffInHours(now()->floorSeconds()) === $schedule->delay_minutes
-            : $schedule->timestamp_target->diffInHours(now()->floorSeconds()) > $schedule->delay_minutes;
+        return $schedule->timestamp_target->diffInMinutes(now()->floorSeconds()) >= $schedule->delay_minutes;
     }
 }
--- tests/Feature/AfterConstraintTest.php
+++ tests/Feature/AfterConstraintTest.php
@@ -10,16 +10,12 @@ use Illuminate\Support\Facades\Mail;
 
 class AfterConstraintTest extends TestCase
 {
-    public function test_past_target_with_after_now_passed_after_constraint_day_bases(): void
+    public function test_past_target_with_after_constraint_day_bases(): void
     {
         Mail::fake();
         Mail::assertNothingSent();
 
-        $scheduler = MailatorSchedule::init('Invoice reminder.')
-            ->recipients($mail = 'zoo@bar.com')
-            ->mailable(
-                (new InvoiceReminderMailable())->to('foo@bar.com')
-            )
+        $scheduler = $this->initScheduler()
             ->days(3)
             ->after(now()->subDay());
 
@@ -28,7 +24,7 @@ class AfterConstraintTest extends TestCase
         $this->travel(1)->days();
 
         self::assertTrue(
-            $scheduler->fresh()->isFutureAction()
+            $scheduler->isFutureAction()
         );
 
         $this->travel(1)->days();
@@ -43,16 +39,12 @@ class AfterConstraintTest extends TestCase
         );
     }
 
-    public function test_past_target_with_after_now_passed_after_constraint_hourly_bases(): void
+    public function test_past_target_with_after_constraint_hourly_bases(): void
     {
         Mail::fake();
         Mail::assertNothingSent();
 
-        $scheduler = MailatorSchedule::init('Invoice reminder.')
-            ->recipients($mail = 'zoo@bar.com')
-            ->mailable(
-                (new InvoiceReminderMailable())->to('foo@bar.com')
-            )
+        $scheduler = $this->initScheduler()
             ->hours(3)
             ->after(now()->subHours(1));
 
@@ -60,38 +52,119 @@ class AfterConstraintTest extends TestCase
 
         $this->travel(1)->hours();
 
+        // An hour before
         self::assertTrue(
-            $scheduler->fresh()->isFutureAction()
+            $scheduler->isFutureAction()
         );
 
         $this->travel(1)->hours();
 
-        $can = app(
-            AfterConstraint::class
-        )->canSend(
-            $scheduler,
-            $scheduler->logs
+        self::assertFalse(
+            $scheduler->isFutureAction()
         );
 
+        // Right now
         self::assertTrue(
-            $can
+            $this->canSend($scheduler)
         );
 
         $this->travel(1)->hours();
 
-        $can = app(
-            AfterConstraint::class
-        )->canSend(
-            $scheduler,
-            $scheduler->logs
+        self::assertFalse(
+            $scheduler->isFutureAction()
         );
 
-        self::assertFalse(
-            $scheduler->fresh()->isFutureAction()
-        );
-
-        self::assertFalse(
-            $can
+        // An hour later
+        self::assertTrue(
+            $this->canSend($scheduler)
         );
     }
+
+    public function test_past_target_with_after_now_passed_after_constraint_hour_bases()
+    {
+        $scheduler = $this->initScheduler()
+            ->hours(2);
+
+        $scheduler->save();
+
+        self::assertFalse(
+            $this->canSend($scheduler)
+        );
+
+        $this->travel(3)->hours();
+
+        self::assertTrue(
+            $this->canSend($scheduler)
+        );
+    }
+
+    public function test_past_target_with_after_now_before_after_constraint_minute_bases()
+    {
+        $scheduler = $this->initScheduler()
+            ->minutes(1);
+        $scheduler->save();
+
+        self::assertTrue(
+            $scheduler->isFutureAction()
+        );
+
+        self::assertFalse(
+            $this->canSend($scheduler)
+        );
+    }
+
+    public function test_past_target_with_after_now_passed_after_constraint_minute_bases()
+    {
+        $scheduler = $this->initScheduler()
+            ->minutes(1);
+        $scheduler->save();
+
+        $this->travel(2)->minutes();
+
+        self::assertFalse(
+            $scheduler->isFutureAction()
+        );
+
+        self::assertTrue(
+            $this->canSend($scheduler)
+        );
+    }
+
+    public function test_past_target_with_after_now_equals_after_constraint_minute_bases()
+    {
+        $scheduler = $this->initScheduler()
+            ->minutes(1);
+        $this->travel(1)->minutes();
+
+        $scheduler->save();
+
+        self::assertFalse(
+            $scheduler->isFutureAction()
+        );
+
+        self::assertTrue(
+            $this->canSend($scheduler)
+        );
+    }
+
+    private function canSend(MailatorSchedule $scheduler)
+    {
+        return  app(
+            AfterConstraint::class
+        )->canSend(
+            $scheduler,
+            $scheduler->logs
+        );
+    }
+
+    private function initScheduler(): MailatorSchedule
+    {
+        return MailatorSchedule::init('Invoice reminder.')
+            ->recipients($mail = 'zoo@bar.com')
+            ->mailable(
+                (new InvoiceReminderMailable())->to('foo@bar.com')
+            )
+            ->after(now());
+    }
+
 }
